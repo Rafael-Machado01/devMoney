e79425389721f0a2cd60f7788503a264e112ef78
Rafael <rafael.machado.ampudiabr@gmail.comgit config --global user.email rafael.machado.ampudiabr@gmail.com>
2022-06-17 18:44:46 -0300

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./assets/styles/global";
+export function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
