93cb0276d65e26f4ccb9014ea7518948d670c3d6
Rafael-Machado01 <rafael.machado.ampudia01@gmail.com>
2022-07-01 13:16:29 -0300

    Error css button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
-import { Container } from "./style";
+import { Container, RadioButton, TransactionTypeContainer } from "./style";
 import CloseImg from "../../assets/close.svg";
-
+import IncomeImg from "../../assets/income.svg";
+import OutcomeImg from "../../assets/outcome.svg";
+import { useState } from "react";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -11,6 +13,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
   return (
     <Modal
       isOpen={isOpen}
@@ -29,6 +32,26 @@ export function NewTransactionModal({
         <h2>Cadastrar transação</h2>
         <input placeholder="Titulo" />
         <input type="number" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <RadioButton
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+          >
+            <img src={IncomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioButton>
+          <RadioButton
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+          >
+            <img src={OutcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioButton>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from "polished";
 export const Container = styled.form`
   h2 {
     color: var(--text-title);
@@ -39,5 +40,40 @@ export const Container = styled.form`
     &:hover {
       filter: brightness(0.9);
     }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
+export const RadioButton = styled.button`
+  button[type="button"] {
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: filter, border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, "#d7d7d7")};
+      cursor: pointer;
+    }
+  }
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
